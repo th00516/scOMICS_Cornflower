b31dd51faf46dd1fb7b3d2a880aa2ee7e5fa3f63
yuhao <yuhao819@hotmail.com>
2019-10-22 23:52:22 +0800

    update string's algorithm of compression

--- utils/largeList2matrix.py
+++ utils/largeList2matrix.py
@@ -9,46 +9,45 @@ DATE:   2019-10-15
 
 from sys import argv
 from os.path import basename
-from gzip import open as gopen
-from lzma import (compress, decompress)  # 'lzma' can provide a high compressibility
-# from lz4.block import (compress, decompress)  # 'lz4' can provide a faster compression speed
+from gzip import (open as gopen, compress as gcompress)
+from lz4.block import (compress as lcompress, decompress as ldecompress)
 from numpy import (asarray, char)
 
 
 def largeList2matrix(largeList_path):
     """"""
-    with open(basename(largeList_path) + '.csv.lz', 'w+b') as OFH:
+    with open(basename(largeList_path) + '.csv.gz', 'w+b') as OFH:
         with gopen(largeList_path, 'rb') as IFH:
             IFH.readline()
             IFH.readline()
-            max_row, max_col, _ = IFH.readline().decode(encoding='utf-8').split()
+            max_row, max_col, _ = IFH.readline().decode().split()
             max_col = int(max_col)
 
             ####################################################
             # Generate a frame for accommodating all the value #
             ####################################################
-            new_line = compress(''.join(('0.00e+00,' * (max_col - 1), '0.00e+00\n')).encode(encoding='utf-8'))
+            new_line = lcompress(''.join(('0.00e+00,' * (max_col - 1), '0.00e+00\n')).encode())
             ####################################################
 
             buffer = IFH.read(209715200)  # IO buffer = 200M
             while buffer:
-                buffer = buffer.decode(encoding='utf-8')
+                buffer = buffer.decode()
                 while buffer[-1] != '\n':
-                    buffer += IFH.read(1).decode(encoding='utf-8')
+                    buffer += IFH.read(1).decode()
 
                 n = 1
-                frameBuf = decompress(new_line).decode(encoding='utf-8').split(',')
+                frameBuf = ldecompress(new_line).decode().split(',')
                 for row, col, val in asarray(list(char.split(asarray(buffer.split('\n')[:-1])))):
                     row = int(row)
                     col = int(col)
 
                     if row > n:
-                        OFH.write(compress(''.join(','.join(frameBuf)).encode(encoding='utf-8')))
+                        OFH.write(gcompress(''.join(','.join(frameBuf)).encode()))
                         n += 1
-                        frameBuf = decompress(new_line).decode(encoding='utf-8').split(',')
+                        frameBuf = ldecompress(new_line).decode().split(',')
 
                     frameBuf[col - 1] = val
-                OFH.write(compress(''.join(','.join(frameBuf)).encode(encoding='utf-8')))
+                OFH.write(gcompress(''.join(','.join(frameBuf)).encode()))
 
                 buffer = IFH.read(209715200)
 

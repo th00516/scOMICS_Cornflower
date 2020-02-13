11f8959f3ebb07b5d448e33dc329023ccde2b6ab
yuhao <yuhao819@hotmail.com>
2020-02-13 13:02:57 +0800

    完成框架搭建

--- src/view/script/script.main.js
+++ src/view/script/script.main.js
@@ -1,8 +1,17 @@
-﻿$(document).ready(function(){
+﻿/*eslint-env jquery*/
+
+$(document).ready(function(){
 
   $("#year").html(function(){
     var thisYear = new Date();
     return thisYear.getFullYear();
+  });
+  
+  $("#about").click(function(){
+    alert(
+"这是一个关于单细胞组学的数据分析平台，\n\
+我们立志于打造一个功能丰富且易用的scOmics分析工具"
+    );
   });
 
 });

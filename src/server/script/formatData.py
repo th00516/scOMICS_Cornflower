c9116394532da2f676fd142021724cd2ad33ce4d
Hao Yu <yuhao819@hotmail.com>
2020-03-13 10:09:16 +0800

    创建后端

---
+++ src/server/script/formatData.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+import sys
+import mysql.connector
+
+
+class dataModel:
+    """"""
+
+    def __init__(self):
+        """"""
+        pass
+
+    def parseInputFile(self):
+        """"""
+        pass
+
+    def exportDB(self):
+        """"""
+        pass

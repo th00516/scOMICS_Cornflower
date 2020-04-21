f508bbabf5112f802da6bd5e1bc0db62c1124cce
yuhao <yuhao819@hotmail.com>
2020-04-21 14:20:06 +0800

    更新CDCP，修补性能Bug，避免重复计算

--- src/CDCP/action/basicComparison.py
+++ src/CDCP/action/basicComparison.py
@@ -4,6 +4,7 @@
 
 
 import numpy as np
+from datatable import f
 
 
 
@@ -17,31 +18,12 @@ class Parser:
         self.METADATA = metadataObj
 
 
-        self.FIELD_FEATURE = {
-
-            'maxGeneExp': 0,
-            'minGeneExp': 0,
-            'aveGeneExp': 0,
-            'numExpCell': 0,
-            'posExpRate': 0
-
-        }
-
-
-    def getFieldFeature(self, fieldName):
-        """"""
-
-        self.FIELD_FEATURE['maxGeneExp'] = self.METADATA.DATATABLE[fieldName].max()[0, 0]
-        self.FIELD_FEATURE['minGeneExp'] = self.METADATA.DATATABLE[fieldName].min()[0, 0]
-        self.FIELD_FEATURE['aveGeneExp'] = self.METADATA.DATATABLE[fieldName].mean()[0, 0]
-        self.FIELD_FEATURE['numExpCell'] = np.nonzero(self.METADATA.DATATABLE[fieldName].to_numpy())[0].size
-        self.FIELD_FEATURE['posExpRate'] = self.FIELD_FEATURE['numExpCell'] / self.METADATA.FEATURE['numCell']
-
-
     def compareFields(self, fieldName1, fieldName2):
         """"""
 
-        posOrNeg = self.METADATA.DATATABLE[fieldName1].to_numpy() * self.METADATA.DATATABLE[fieldName2].to_numpy()
-        posOrNeg[posOrNeg > 0] = 1
-        
-        self.METADATA.DATATABLE[fieldName1 + '/' + fieldName2] = posOrNeg
+        if fieldName1 + '/' + fieldName2 not in self.METADATA.DATATABLE.keys():
+
+            posOrNeg = self.METADATA.DATATABLE[fieldName1].to_numpy() * self.METADATA.DATATABLE[fieldName2].to_numpy()
+            posOrNeg[posOrNeg > 0] = 1
+            
+            self.METADATA.DATATABLE[fieldName1 + '/' + fieldName2] = posOrNeg
